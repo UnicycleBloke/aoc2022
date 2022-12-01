1faf8403077e4a1b834e27ec111f0860b395a27a
Alan Chambers <unicycle.bloke@gmail.com>
2022-12-01 05:26:26 +0000

    Finished Day01

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -1,31 +1,67 @@
 #include "utils.h"
 
 
-auto part1(const auto& input)
+template <typename T>
+auto part1(const T& input)
 {
     aoc::timer timer;
-    return 0;
+
+    int res = 0;
+    int sum = 0;
+    for (const auto& line: input)
+    {
+        if (line.size() == 0)
+        {
+            res = max(res, sum);
+            sum = 0; 
+        }
+        else
+        {
+            sum += stoi(line);
+        }
+    }
+
+    return res;
 }
 
 
-auto part2(const auto& input)
+template <typename T>
+auto part2(const T& input)
 {
     aoc::timer timer;
-    return 0;
+
+    vector<int> res;
+    int sum = 0;
+    for (const auto& line: input)
+    {
+        if (line.size() == 0)
+        {
+            res.push_back(sum);
+            sum = 0; 
+        }
+        else
+        {
+            sum += std::stoi(line);
+        }
+    }
+    res.push_back(sum);
+
+    sort(res.begin(), res.end(), greater<int>());
+    return res[0] + res[1] + res[2];
 }
 
 
 void run(const char* filename)
 {
-    auto lines = aoc::read_lines(filename, false); 
+    auto lines = aoc::read_lines(filename, true);     
 
     auto p1 = part1(lines);
     cout << "Part1: " << p1 << '\n';
-    //aoc::check_result(p1, 5218);
+    aoc::check_result(p1, 75501);
 
     auto p2 = part2(lines);
     cout << "Part2: " << p2 << '\n';
-    //aoc::check_result(p2, 15527);
+    aoc::check_result(p2, 215594);
 }
 
 
