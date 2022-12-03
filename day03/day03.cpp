f14d6d234172ba0a6243d7bacc075b3b91cb3177
Alan Chambers <unicycle.bloke@gmail.com>
2022-12-03 05:29:06 +0000

    Completed Day03.

--- day03/day03.cpp
+++ day03/day03.cpp
@@ -5,7 +5,25 @@ template <typename T>
 auto part1(const T& input)
 {
     aoc::timer timer;
-    return 0;
+
+    int result = 0;
+    for (const auto& s: input)
+    {
+        auto len = s.size();
+        auto a   = s.substr(0, len/2);
+        auto b   = s.substr(len/2, len/2);
+
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+
+        decltype(a) c;
+        set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(c));
+
+        if (c[0] >= 'a' && c[0] <= 'z') result += c[0] - 'a' + 1;
+        if (c[0] >= 'A' && c[0] <= 'Z') result += c[0] - 'A' + 27;
+    }
+
+    return result;
 }
 
 
@@ -13,21 +31,43 @@ template <typename T>
 auto part2(const T& input)
 {
     aoc::timer timer;
-    return 0;
+
+    int result = 0;
+    for (auto it = input.begin(); it != input.end();)
+    { 
+        auto a = *it++;   
+        auto b = *it++;   
+        auto c = *it++;   
+
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        sort(c.begin(),c.end());
+
+        decltype(a) d;
+        set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(d));
+
+        decltype(a) e;
+        set_intersection(d.begin(), d.end(), c.begin(), c.end(), back_inserter(e));
+
+        if (e[0] >= 'a' && e[0] <= 'z') result += e[0] - 'a' + 1;
+        if (e[0] >= 'A' && e[0] <= 'Z') result += e[0] - 'A' + 27;
+    }
+
+    return result;
 }
 
 
 void run(const char* filename)
 {
-    auto lines = aoc::read_lines(filename, true);     
+    auto lines = aoc::read_lines(filename, false);     
 
     auto p1 = part1(lines);
     cout << "Part1: " << p1 << '\n';
-    //aoc::check_result(p1, 0);
+    aoc::check_result(p1, 8105);
 
     auto p2 = part2(lines);
     cout << "Part2: " << p2 << '\n';
-    //aoc::check_result(p2, 0);
+    aoc::check_result(p2, 2363);
 }
 
 
