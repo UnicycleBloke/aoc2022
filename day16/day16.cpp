1443976a89ad2e9d7cf10c0b216a6e480f5f0488
Alan Chambers <unicycle.bloke@gmail.com>
2022-12-18 16:25:09 +0000

    Completed Day16 - finally after much faff

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -7,21 +7,49 @@ struct State
     set<string> open{};
     int         total{};
     int         rate{};
-    int         delta{};
+    int         delay{};
 };
+
+
+struct State2
+{
+    // Shared
+    set<string> open{};
+    int         total{};
+    int         rate{};
+    // Me 
+    string      i_valve{"AA"};
+    int         i_delay{};
+    // The elephant
+    string      e_valve{"AA"};
+    int         e_delay{};
+};
+
 
 bool operator<(const State& lhs, const State& rhs)
 {
     if (lhs.total > rhs.total) return true;
-    //if (lhs.possible > rhs.possible) return true;
-    // if (lhs.rate > rhs.rate) return true;
-    // if ((lhs.rate == rhs.rate) && (lhs.total > rhs.total)) return true;
-    return false;;
+    return false;
+}
+
+bool operator<(const State2& lhs, const State2& rhs)
+{
+    if (lhs.total > rhs.total) return true;
+    return false;
+}
+
+
+int cost(map<pair<string, string>, int>& edges, const string& from, const string& to)
+{
+    auto e = pair{from, to};                 
+    if (e.second < e.first)
+        e = pair{e.second, e.first};  
+    return edges[e];
 }
 
 
 template <typename T>
-auto part1(T& input, string first)
+auto part1(T& input, string first, map<pair<string, string>, int>& edges)
 {
     aoc::timer timer;
  
@@ -29,109 +57,286 @@ auto part1(T& input, string first)
     State start;
     start.valve = first;
     states.push_back(start);
-    cout << first << "\n";
 
     for (int minute: aoc::range(1, 31))
     {
         vector<State> states2;
 
-        cout << "\nMinute " << minute <<" size=" << states.size() << "\n\n";
-
         for (const auto& state: states)
         {
-            const auto& [valve, rate, valves] = input[state.valve];
-            bool open = state.open.find(valve) != state.open.end();
+            const auto& [rate, valves] = input[state.valve];
+            bool open = state.open.find(state.valve) != state.open.end();
 
-            if (!open && (rate > 0))
+            if (state.delay > 0)
             {
                 State state2 = state;
                 state2.total += state2.rate;
-
-                state2.open.insert(valve);
-                state2.delta  = rate;
-                state2.rate  += rate;
-
+                --state2.delay;
                 states2.push_back(state2);
-
-                // if (valve == string{"JJ"})
-                // {
-                //     cout << state.valve << " opened " << state2.rate << " " << state2.total << " (";
-                //     for (const auto& o: state2.open)
-                //         cout << o << " ";
-                //     cout << ")\n";
-                // }
             }
-
-            for (const auto& valve2: valves)
+            else
             {
-                State state2  = state;
-                state2.delta  = 0;
-                state2.total += state2.rate;
-                state2.valve  = valve2;
+                if (!open && (rate > 0))
+                {
+                    State state2 = state;
+                    state2.total += state2.rate;
+                    state2.open.insert(state.valve);
+                    state2.rate  += rate;
+                    states2.push_back(state2);
+                }
 
-                states2.push_back(state2);
-
-                // cout << state.valve << " => " << state2.valve << " " << state2.rate << " " << state2.total << " (";
-                // for (const auto& o: state2.open)
-                //     cout << o << " ";
-                // cout << ")\n";
+                for (const auto& valve2: valves)
+                {
+                    State state2  = state;
+                    state2.total += state2.rate;
+                    state2.valve  = valve2;
+                    state2.delay  = cost(edges, state.valve, valve2) - 1;
+                    states2.push_back(state2);
+                }
             }
         }
 
-        constexpr int SIZE = 100000;
+        constexpr int SIZE = 5'000;
 
-        sort(states2.begin(), states2.end());
-        if (states2.size() > SIZE)
-            states2.erase(states2.begin() + SIZE, states2.end());
         states = std::move(states2);
-
-        cout << "Minute " << minute << " " << states[0].rate << " " << states[0].total << " ";
-        cout << "( ";
-        for (const auto& o: states[0].open)
-            cout << o << " ";
-        cout << ")\n";
+        sort(states.begin(), states.end());
+        if (states.size() > SIZE)
+            states.erase(states.begin() + SIZE, states.end());
     }
 
-    return 0;
+    return states[0].total;
 }
 
 
 template <typename T>
-auto part2(T& input)
+auto part2(T& input, string first, map<pair<string, string>, int>& edges)
 {
     aoc::timer timer;
-    return 0;
+ 
+    vector<State2> states;
+    State2 start;
+    start.i_valve = first;
+    start.e_valve = first;
+    states.push_back(start);
+
+    for (int minute: aoc::range(1, 27))
+    {
+        vector<State2> states2;
+
+        auto elephant = [&](const State2& state)
+        {
+            const auto& [rate, valves] = input[state.e_valve];
+            bool open = state.open.find(state.e_valve) != state.open.end();
+
+            if (state.e_delay > 0)
+            {
+                State2 state2 = state;
+                //state2.total += state2.rate;
+                --state2.e_delay;
+                states2.push_back(state2);
+            }
+            else
+            {
+                if (!open && (rate > 0))
+                {
+                    State2 state2 = state;
+                    //state2.total += state2.rate;
+                    state2.open.insert(state.e_valve);
+                    state2.rate  += rate;
+                    states2.push_back(state2);
+                }
+
+                for (const auto& valve2: valves)
+                {
+                    State2 state2  = state;
+                    //state2.total += state2.rate;
+                    state2.e_valve  = valve2;
+                    state2.e_delay  = cost(edges, state.e_valve, valve2) - 1;
+                    states2.push_back(state2);
+                }
+            }
+        };
+
+        auto user = [&](const State2& state)
+        {
+            const auto& [rate, valves] = input[state.i_valve];
+            bool open = state.open.find(state.i_valve) != state.open.end();
+
+            if (state.i_delay > 0)
+            {
+                State2 state2 = state;
+                state2.total += state2.rate;
+                --state2.i_delay;
+                elephant(state2);
+            }
+            else
+            {
+                if (!open && (rate > 0))
+                {
+                    State2 state2 = state;
+                    state2.total += state2.rate;
+                    state2.open.insert(state.i_valve);
+                    state2.rate  += rate;
+                    elephant(state2);
+                }
+
+                for (const auto& valve2: valves)
+                {
+                    State2 state2  = state;
+                    state2.total += state2.rate;
+                    state2.i_valve  = valve2;
+                    state2.i_delay  = cost(edges, state.i_valve, valve2) - 1;
+                    elephant(state2);
+                }
+            }
+        };
+
+        for (const auto& state: states)        
+        {
+            // Compose the action of the the user (me) and the elephant the user 
+            // calling the elephant with an updated state for each new path, and the 
+            // elephant then appending its modified versions of that to the state vector.
+            // This kind of squares the number of expected states for each minute.
+            user(state);
+        }
+
+        constexpr int SIZE = 5'000;
+
+        states = std::move(states2);
+        sort(states.begin(), states.end());
+        if (states.size() > SIZE)
+            states.erase(states.begin() + SIZE, states.end());
+    }
+
+    return states[0].total;
 }
 
 
-void run(const char* filename)
+// Remove the nodes with zero rate to reduce the massive explosion of paths we will get to 
+// a manageable scale. Replace the intermediate nodes with a cost of moving along each edge.
+// It took a ridiculous amount of time to get the logic for this right - my brain was in
+// hibernation during a cold snap, or something.
+auto reduce(const char* filename)
 {
-    using Input = tuple<int, vector<string>>;
-    map<string, Input> input;
+    map<pair<string, string>, int> edges;
+    map<string, int> rates;
 
     auto lines = aoc::read_lines(filename);
     for (auto line: lines)
     {
         line = aoc::replace(line, ", ", ",");
-        auto [valve, rate, vs] = aoc::parse_line<string, int, string>(R"(Valve (\w\w) has flow rate=(\d+); tunnels? leads? to valves? (.*))", line);
+        auto [v1, rate, vs] = aoc::parse_line<string, int, string>(R"(Valve (\w\w) has flow rate=(\d+); tunnels? leads? to valves? (.*))", line);
         auto valves = aoc::split(vs, ",", false);
-        input[valve] = make_tuple(rate, valves);
+
+        for (const auto& v2: valves)
+        {
+            if (v1 < v2)
+                edges[{v1, v2}] = 1;
+            else
+                edges[{v2, v1}] = 1;
+        }
+        rates[v1] = rate;
     }
 
-    // for (const auto& [valve, value]: input)
-    // {
-    //     const auto& [rate, valves] = value;
-    //     cout << valve << " " << rate << " "; 
-    //     for (auto v: valves)
-    //         cout << v << " ";
-    //     cout << "\n";
-    // }
+    auto combine = [&] 
+    {
+        for (const auto& [e1, c1]: edges)
+        {
+            if ((rates[e1.first] == 0) && (rates[e1.second] == 0)) continue;
+            if ((rates[e1.first] != 0) && (rates[e1.second] != 0)) continue;
+            // One and only one end is a zero
 
+            for (const auto& [e2, c2]: edges)
+            {
+                if (e1 == e2) continue;
+                if ((rates[e2.first] != 0) && (rates[e2.second] != 0)) continue;
+                // Either end could be a zero
+
+                pair<string, string> e;
+                bool match = false;
+
+                if ((rates[e1.first] == 0) && (rates[e2.first] == 0) && (e1.first == e2.first))
+                {
+                    e = pair{e1.second, e2.second}; 
+                    match = true;
+                }
+
+                else if ((rates[e1.second] == 0) && (rates[e2.first] == 0) && (e1.second == e2.first))
+                {
+                    e = pair{e1.first, e2.second};  
+                    match = true;
+                }
+
+                else if ((rates[e1.first] == 0) && (rates[e2.second] == 0) && (e1.first == e2.second))
+                {
+                    e = pair{e1.second, e2.first}; 
+                    match = true;
+                }
+
+                else if ((rates[e1.second] == 0) && (rates[e2.second] == 0) && (e1.second == e2.second))
+                {
+                    e = pair{e1.first, e2.first};                 
+                    match = true;
+                }
+
+                if (!match) continue;  
+
+                edges.erase(e1);
+                edges.erase(e2);
+
+                if (e.second < e.first)
+                    e = pair{e.second, e.first};  
+                edges[e] = c1 + c2;
+                return true;
+            }
+        }
+        return false;
+    };
+
+    // To prevent removal.
+    rates["AA"] = 1;
+    int removed = 0;
+    while (combine())
+        ++removed;
+    rates["AA"] = 0;
+
+    return pair{edges, rates};
+} 
+
+
+struct Cave
+{
+    int rate{};
+    vector<string> valves{};
+};
+
+
+void run(const char* filename)
+{
+    auto [edges, rates] = reduce(filename);
+
+    map<string, Cave> input;
+
+    for (const auto& [edge, cost]: edges)
+    {
+        const auto& [v1, v2] = edge;
+        auto rate = rates[v1];
+
+        auto& data1 = input[v1];
+        data1.rate  = rates[v1];
+        data1.valves.push_back(v2);
+
+        auto& data2 = input[v2];
+        data2.rate  = rates[v2];
+        data2.valves.push_back(v1);
+    }
+
+    // Graphviz DOT graph
     // for (const auto& [valve, value]: input)
     // {
     //     const auto& [rate, valves] = value;
     //     for (auto v: valves)
-    //         cout << valve << " -> " << v << ";\n";
+    //         cout << valve << " -> " << v << " [label=\"" << cost(edges, valve, v) << "\"];\n";
     //     cout << valve << " [label=\"" << valve << " " << rate << "\"";
     //     if (rate > 0)
     //     {
@@ -139,45 +344,15 @@ void run(const char* filename)
     //     }
     //     cout << "];";
     //     cout << "\n";
-    // }
-    
-    map<pair<string, string>, int> links;
-    for (const auto& [v1, value1]: input)
-    {
-        for (const auto& [v2, value2]: input)
-        {
-            links[{v1, v2}] = 1'000'000; 
-        }
- 
-        const auto& [rate1, valves1] = value1;
-        for (auto v2: valves1)
-        {
-            links[{v1, v2}] = 1; 
-        }
-    }
+    // }    
 
-    while (true)
-    {
-        for (const auto& [from, value]: input)
-        {
-            const auto& [rate, valves] = value;
-            for (auto to: valves)
-            {
-                int d = links;
-            }
-                links[{from, to}] = 1; 
-        }
-    }
+    auto p1 = part1(input, "AA", edges);
+    cout << "Part1: " << p1 << '\n';
+    aoc::check_result(p1, 2077);
 
-
-
-    // auto p1 = part1(input, start);
-    // cout << "Part1: " << p1 << '\n';
-    // //aoc::check_result(p1, 0);
-
-    // auto p2 = part2(input);
-    // cout << "Part2: " << p2 << '\n';
-    //aoc::check_result(p2, 0);
+    auto p2 = part2(input, "AA", edges);
+    cout << "Part2: " << p2 << '\n';
+    aoc::check_result(p2, 2741);
 }
 
 
